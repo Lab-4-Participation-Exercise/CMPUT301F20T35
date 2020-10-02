85bfacf6c060cbed1f2d51ccaea8a8fe196f39ab
zhengyao603 <zhengyao@ualberta.ca>
2020-10-01 19:52:04 -0600

    update

--- app/src/main/java/com/example/cmput301f20t35/Shape.java
+++ app/src/main/java/com/example/cmput301f20t35/Shape.java
@@ -3,6 +3,6 @@ package com.example.cmput301f20t35;
 public abstract class Shape {
     int x;
     int y;
-    String color="Maroon";
+    String color="Blue";
 
 }
--- app/src/main/java/com/example/cmput301f20t35/Triangle.java
+++ app/src/main/java/com/example/cmput301f20t35/Triangle.java
@@ -1,5 +1,4 @@
 package com.example.cmput301f20t35;
 
 public class Triangle extends Shape{
-    String color = "Black";
 }
