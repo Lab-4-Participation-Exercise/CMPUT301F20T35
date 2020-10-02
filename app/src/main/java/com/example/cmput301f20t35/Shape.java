2dfe36c7244702d500d54f2805825dc124a66121
SHAWNXU <72234348+shawnxxxu@users.noreply.github.com>
2020-10-02 10:50:04 +0800

    Update Shape.java

--- app/src/main/java/com/example/cmput301f20t35/Shape.java
+++ app/src/main/java/com/example/cmput301f20t35/Shape.java
@@ -3,9 +3,6 @@ package com.example.cmput301f20t35;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< Updated upstream
-
-=======
     String color ="grey";
->>>>>>> Stashed changes
+
 }
